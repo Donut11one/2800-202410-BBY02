4cf7d1b4e071f5b1ee8e17134afa571b51757b8e
Nickolay Makarenko <misterzero2001.com@gmail.com>
2024-05-14 00:52:32 -0700

    Migrated from Truffle JS to HardHat, Changed config, Smart-Contract Security Update and Extended functionality

--- contracts/DocuMinter.sol
+++ contracts/DocuMinter.sol
@@ -4,14 +4,18 @@ pragma solidity ^0.8.8;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+error DocuMinter__NoDocumentsMinted();
+error DocuMinter__NotAuthorizedToOperate();
+
 //Mint NFT from passed URI
 //transfer documents ownership
-//Improve token URI storage
+//Burn NFTs
 contract DocuMinter is Ownable, ERC721 {
     uint256 private s_tokenCounter;
     using Strings for uint256;
     //URI accsess
     mapping(uint256 => string) private _tokenURIs;
+    mapping(address => uint256[]) private belongings;
 
     string private _baseURIextended;
 
@@ -19,12 +23,11 @@ contract DocuMinter is Ownable, ERC721 {
         s_tokenCounter = 0;
     }
 
-    //because it's Public anyone can see the document details
     //We need to decide wether onlyOwner will see the URI to display on the website or make it public to anyone
     //assumes that token ID exists. For improvement needs validation that Id exists
     function tokenURI(
         uint256 tokenId
-    ) public view virtual override returns (string memory) {
+    ) public view virtual override onlyOwner returns (string memory) {
         string memory _tokenURI = _tokenURIs[tokenId];
         string memory base = _baseURIextended;
         // If there is no base URI, return the token URI.
@@ -51,15 +54,85 @@ contract DocuMinter is Ownable, ERC721 {
         _tokenURIs[tokenId] = _tokenURI;
     }
 
-    function mintDoc(string memory _tokenURI) public returns (uint256) {
+    /**
+     * Transfers the ownership of the NFT
+     * @param _receiver Address of the receiver
+     * @param _tokenId An ID of the NFT
+     */
+    function conductTransfer(
+        //CHANGE IDS IN THE MAPPINGS
+        address _receiver,
+        uint256 _tokenId
+    ) external {
+        if (ownerOf(_tokenId) == msg.sender) {
+            _safeTransfer(msg.sender, _receiver, _tokenId);
+            removeValue(msg.sender, _tokenId);
+            belongings[_receiver].push(_tokenId);
+        } else {
+            revert DocuMinter__NotAuthorizedToOperate();
+        }
+    }
+
+    /**
+     * Can be called only by the owner.
+     * @param ownerAddress address of the caller
+     * @return belongings[ownerAddress] List of all NFTs' ids owned by the user
+     */
+    function owns(address ownerAddress) public view returns (uint256[] memory) {
+        if (ownerAddress == msg.sender) {
+            if (belongings[ownerAddress].length > 0) {
+                return belongings[ownerAddress];
+            }
+            revert DocuMinter__NoDocumentsMinted();
+        } else {
+            revert DocuMinter__NotAuthorizedToOperate();
+        }
+    }
+
+    /**
+     *
+     * @param _tokenURI  URI with the metadata
+     * @return  newId The id of the recently created NFT
+     */
+    function mintDoc(string memory _tokenURI) external returns (uint256) {
         s_tokenCounter += 1;
         uint256 newId = s_tokenCounter;
+        belongings[msg.sender].push(newId);
         _safeMint(msg.sender, newId);
         _setTokenURI(newId, _tokenURI);
         return newId;
     }
 
-    function totalMinted() public view returns (uint256) {
+    /**
+     * Deletes the NFT by sending it to the null address
+     * @param _tokenId An ID of the NFT
+     */
+    function burnDoc(uint256 _tokenId) external {
+        if (ownerOf(_tokenId) == msg.sender) {
+            _burn(_tokenId);
+            delete _tokenURIs[_tokenId];
+            removeValue(msg.sender, _tokenId);
+        } else {
+            revert DocuMinter__NotAuthorizedToOperate();
+        }
+    }
+
+    function removeValue(address user, uint256 value) internal returns (bool) {
+        for (uint256 i = 0; i < belongings[user].length; i++) {
+            if (belongings[user][i] == value) {
+                // Move the last element to the position of the element to delete
+                belongings[user][i] = belongings[user][
+                    belongings[user].length - 1
+                ];
+                // Remove the last element
+                belongings[user].pop();
+                return true;
+            }
+        }
+        return false; // Value not found
+    }
+
+    function totalMinted() external view returns (uint256) {
         return s_tokenCounter;
     }
 }
--- scripts/runScript.js
+++ scripts/runScript.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const pinFileToIPFS = require('./pinFileToIPFS');
-// const filePath = path.join(__dirname, '../assets/4e1l81.jpg');
+// const filePath = path.join(__dirname, '../assets/test.jpg');
 const filePath = path.join(__dirname, '../data/metadata.json');
 pinFileToIPFS(filePath);
+
+//pin image -> pin metadata -> smart-contract
+//connects wallet -> Our platform pins the image (User waits) -> User has a mint button

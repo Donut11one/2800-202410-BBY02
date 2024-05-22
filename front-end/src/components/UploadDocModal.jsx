b9525b8229a2b8f23c0d466b12ed65506023b560
Alex He <donut11one@gmail.com>
2024-05-22 14:24:10 -0700

    minor bug fix to UploadDocModal.jsx

--- front-end/src/components/UploadDocModal.jsx
+++ front-end/src/components/UploadDocModal.jsx
@@ -2,19 +2,19 @@ import { ethers } from "ethers";
 import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../assets/helper-hardhat-config.js";
 import { Web3Provider } from "@ethersproject/providers";
 
-import React, { useState } from "react";
+import { React,  useState } from "react";
 import filehash from "../runScript.js";
 
 const UploadDocModal = ({ onClose }) => {
   const [name, setName] = useState(""); // State for user's name
   const [description, setDescription] = useState(""); // State for description
-  const [file, setFile] = useState(""); // State for file
+  const [image, setImage] = useState(""); // State for file
 
   const handleSubmit = async () => {
     const metadata = {
       name,
       description,
-      file,
+      image,
     };
 
 
@@ -29,32 +29,18 @@ const UploadDocModal = ({ onClose }) => {
   };
 
   const mintNFT = async (jsondata) => {
-    const [tokenIds, setTokenIds] = useState([]);
-    const [tokenURIs, setTokenURIs] = useState([]);
     try {
       // Connect to Ethereum provider
       const provider = new Web3Provider(window.ethereum);
       const signer = provider.getSigner();
 
-
       // Instantiate the contract
       const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
 
-      // Call the smart contract function owns(address)
+      // Call the smart contract function mintDoc to mint document
       const tokenIdsOwned = await contract.mintDoc(jsondata);
 
-      // Set tokenIds state
-      setTokenIds(tokenIdsOwned);
 
-      // Call the smart contract function tokenURI(id) for each token
-      const tokenURIs = await Promise.all(
-        tokenIdsOwned.map(async (id) => {
-          return await contract.tokenURI(id);
-        })
-      );
-
-      // Set tokenURIs state
-      setTokenURIs(tokenURIs);
     } catch (error) {
       console.error("Error fetching token data:", error);
     }
@@ -85,7 +71,7 @@ const UploadDocModal = ({ onClose }) => {
             <input
               type="file"
               id="fileupload"
-              onChange={async (e) => setFile(await filehash(e.target.files[0]))}
+              onChange={async (e) => setImage(await filehash(e.target.files[0]))}
             />
             <button type="button" onClick={handleSubmit}>
               Mint Document

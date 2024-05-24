46f51fe89acb953c2c1bdc93bdc60d9020bc5050
Nickolay Makarenko <misterzero2001.com@gmail.com>
2024-05-23 17:19:42 -0700

    Fixed tailwind

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals.js';
-import { ThemeProvider } from "@material-tailwind/react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+
+    <App />
+
   </React.StrictMode>
 );
 

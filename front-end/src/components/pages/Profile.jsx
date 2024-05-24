834c1875fa92cbe4acb75a9e33dc0034283c88d5
Niki <NIKI@999.com>
2024-05-23 23:33:46 -0700

    update user profile

--- front-end/src/components/pages/Profile.jsx
+++ front-end/src/components/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from "../Button";
 import { db, auth } from "../../fbconfig";
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { onAuthStateChanged } from "firebase/auth";
 import Navbar from "../Navbar";
 import Footer from '../Footer';
@@ -9,14 +9,28 @@ import './styles.css';
 
 
 const Profile = () => {
+
+    const [userId, setUserId] = useState('');
     const [userName, setUserName] = useState('BCIT');
     const [userEmail, setUserEmail] = useState('bc@bcit.ca');
     const [deviceInfo, setDeviceInfo] = useState('');
+    const [updbtn, setUpdbtn] = useState(false);
+
+    const handleNameChange = (event) => {
+        setUserName(event.target.value);
+        setUpdbtn(true);
+    };
+
+    const handleEmailChange = (event) => {
+        setUserEmail(event.target.value);
+        setUpdbtn(true);
+    };
 
     useEffect(() => {
 
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
+                setUserId(user.uid);
                 console.log("Current user ID:", user.uid);
             } else {
 
@@ -40,21 +54,51 @@ const Profile = () => {
 
         return () => unsubscribe();
     }, []);
-
+    const updateUserProfile = async () => {
+        if (userId) {
+            const userDocRef = doc(db, "users", userId);
+            try {
+                await updateDoc(userDocRef, {
+                    name: userName,
+                    email: userEmail
+                });
+                console.log('User profile updated successfully.');
+                setUpdbtn(false); // Reset the update button disable state
+            } catch (error) {
+                console.error("Error updating document: ", error);
+            }
+        }
+    };
     return (
         <>
             <Navbar />
-            <div className="home min-h-screen flex flex-col items-center justify-normal bg-gray-100 p-4">
-                <div className="modal flex items-center justify-center" style={{ zIndex: 100 }}>
-                    <div className="modal-content bg-emerald-950 rounded-lg shadow-lg p-6">
-                        <h1 className="text-lg" style={{ color: "white" }}>Your Profile</h1>
-                        <p className="text-lg" style={{ color: "white" }}>UserName: {userName}</p>
-                        <p className="text-lg" style={{ color: "white" }}>Email: {userEmail}</p>
-                        <p className="text-lg" style={{ color: "white" }}>Browser: {deviceInfo.browser}</p>
-                        <p className="text-lg" style={{ color: "white" }}>DeviceType: {deviceInfo.deviceType}</p><br />
-                        <Button buttonSize={"btn--small"} onClick={self.close()}>Close</Button>
+            {/* <div className="home min-h-screen flex flex-col items-center justify-start bg-gray-100 p-4"> */}
+            <div className="profile-page ">
+                {/* <div className="modal  items-center justify-start" style={{ zIndex: 100 }}> */}
+                <div className='profile-container w-full h-screen flex flex-col items-center justify-center '>
+                    <div className='profile-content  bg-emerald-950 w-full max-w-4xl p-6  rounded-lg shadow-md'>
+                        <h1 className="text-2xl font-bold text-white-800 mb-6">Your Profile</h1>
+                        <div className="profile-field mb-4">
+                            <label className="text-lg text-white-800" >Name:</label>
+                            <input className="input-field  bg-emerald-950 " type="text"  value={userName} onChange={handleNameChange} />
+                        </div>
+                        <div className="profile-field mb-4">
+                            <label className="text-lg text-white-800">Email:</label>
+                            <input className="input-field bg-emerald-950 " type="email" value={userEmail} onChange={handleEmailChange} />
+                        </div>
+                        <div className="profile-field mb-4">
+                            <p className="text-lg text-white-800">Browser: {deviceInfo.browser}</p>
+                        </div>
+                        <div className="profile-field mb-4">
+                            <p className="text-lg text-white-800">Device Type: {deviceInfo.deviceType}</p>
+                        </div>
+                        <div className="flex mt-4">
+                            <Button className="btn--small mr-2" >Close</Button>
+                            <button className="btn--medium" disabled={!updbtn} onClick={updateUserProfile}>Update</button>
+                        </div>
                     </div>
                 </div>
+
             </div>
             <Footer />
         </>
